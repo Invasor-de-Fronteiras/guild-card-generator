a313749fd1c700d9872473518be12cc72a52f3c4
Gustavo Pegoretti <guhhpegoretti@gmail.com>
2022-09-06 16:49:36 -0300

    return buffer from api.py

--- api.py
+++ api.py
@@ -1,23 +1,24 @@
 import generator
-from fastapi import FastAPI
-from pydantic import BaseModel
+from fastapi import FastAPI, Query
+from fastapi.responses import StreamingResponse
 from typing import List
+from io import BytesIO
 
 
-class Body(BaseModel):
-    name: str
-    guild: str
-    record: int
-    url: str
+
 
 
 app = FastAPI()
 
 
-@app.get("/guildcard/")
-async def guild_card(body: Body, items: List[str]):
-    urlimg = generator.generator_jpg(body.url)
+@app.post("/guildcard/")
+async def guild_card(user: str, guild: str, record: int, url: str, items: List[str]= Query(None)):
+    urlimg = generator.generator_jpg(url)
     background = generator.user_image(urlimg)
-    guildcard = generator.generate_guild_card(body.name, body.guild, body.record, items, background)
+    guildcard = generator.generate_guild_card(user, guild, record, items, background)
 
-    return guildcard
+    filtered_image = BytesIO()
+    guildcard.save(filtered_image, "PNG")
+    filtered_image.seek(0)
+
+    return StreamingResponse(filtered_image, media_type="image/png")
