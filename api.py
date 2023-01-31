5b332c6859b48923879bf86dc837a9f7ff986ff3
Rainã Pepe <raina.pepe@outlook.com>
2023-01-31 20:52:54 -0300

    feat: create dockerfile

--- api.py
+++ api.py
@@ -4,12 +4,7 @@ from fastapi.responses import StreamingResponse
 from typing import List
 from io import BytesIO
 
-
-
-
-
 app = FastAPI()
-
 
 @app.post("/guildcard/")
 async def guild_card(user: str, guild: str, record: int, url: str, items: List[str]= Query(None)):
